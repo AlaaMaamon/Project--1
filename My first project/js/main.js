be8fe245a48b4351103f465a5e42484cda2e5956
AlaaMaamon <110562282+AlaaMaamon@users.noreply.github.com>
2023-01-20 17:27:57 +0200

    Delete My first project directory

--- My first project/js/main.js
+++
@@ -1,9 +0,0 @@
-// Make mobile navigation work
-
-const btn_nav = document.querySelector(".btn-mobile-nav");
-console.log(btn_nav)
-const header = document.querySelector(".header");
-console.log(header)
-btn_nav.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
-});
